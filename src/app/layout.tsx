caed7090d98b0441e657672de2f74817e880d7e6
Fajar Ramdani <108250746+ajay0896@users.noreply.github.com>
2025-12-05 19:38:49 +0700

    update ui user

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import PushNotificationsProvider from "@/components/notifications/PushNotificationsProvider";
-import { UserProvider } from "@/utils/useCurrentUser";
-import DarkThemeProvider from "@/components/ThemeProvider";
-import DarkThemeProvider from "@/components/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
